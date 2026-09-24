67e3977627489a6e28e8a7e3d863d8e3c909b9d7
franferrazdev <franferraz.dev@gmail.com>
2026-09-23 21:41:11 -0300

    remove import footer from main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Footer } from "@/modules/grades/presentation/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
